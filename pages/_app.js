8459066674c6ffe51825ea84dcd1fcd721157a73
BoWang <bbw1996@qq.com>
2021-12-23 23:56:49 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Head>
+      <title>我的博客-BOBO</title>
+    </Head>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
